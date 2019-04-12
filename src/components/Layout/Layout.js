6e709e90a3b5a03ab4951bea43826a6bcde734ca
amit.shah <amit.shah@avista.com>
2019-04-12 18:17:52 +0530

    Episode: 150

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.css';
 
 const layout = (props) => (
     <Aux>
         <div>Toolbar, sidebar, backdrop</div>
-        <main>
+        <main className={classes.content}>
             {props.children}
         </main>
     </Aux>
