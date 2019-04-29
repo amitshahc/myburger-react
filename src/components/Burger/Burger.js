4ae8290636447ee128933f1446d3b1f121c4fd3b
amit.shah <amit.shah@avista.com>
2019-04-29 18:26:23 +0530

    Episode: 156
    * checking length of ingredients if 0

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,15 +4,21 @@ import BurgerIngredient from './BurgerIngrediants/BurgerIngredient';
 
 const burger = (props) => {
 
-    const orderedIngredients = Object.keys(props.ingredients)
+    let orderedIngredients = Object.keys(props.ingredients)
         .map( (item, index) => {
             // console.log("Outer", item, index, props.ingredients[item]);
             return [...Array(props.ingredients[item])].map((v, i) => {
                 // console.log("Inner", v, i);
                 return <BurgerIngredient key={item+i} type={item} /> 
             });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
     
+    if (orderedIngredients.length === 0) {
+        orderedIngredients = <p>Please start adding ingredients</p>;
+    }
     // console.log(orderedIngredients);
     return (
         <div className={classes.Burger}>
