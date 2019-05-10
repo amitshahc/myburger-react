c4f19edb8d0b1437616d8f252e35263fcc7fe1af
amit.shah <amit.shah@avista.com>
2019-05-10 12:01:30 +0530

    Episode: 160 (Executable)

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,9 +5,16 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 
 class BurgerBuilder extends Component {
-
+    
     constructor(props) {
         super(props);
+
+        this.INGREDIENT_PRICES = {
+            "salad" : 0.5,
+            "bacon" : 0.4,
+            "cheese" : 1.3,
+            "meat" : 0.7,  
+        }
 
         this.state = {
             ingredients: {
@@ -15,15 +22,30 @@ class BurgerBuilder extends Component {
                 bacon: 0,
                 cheese: 0,
                 meat: 0
-            }
+            },
+            total_price: 4,
         }
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceAdition = this.INGREDIENT_PRICES[type];
+        const oldPrice = this.state.total_price;
+        const newPrice = oldPrice + priceAdition;
+
+        this.setState({total_price: newPrice, ingredients: updatedIngredients})
     }
 
     render() {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
-                <BuildControls />
+                <BuildControls ingredientAdded={this.addIngredientHandler} />
             </Aux>
         )
     }
